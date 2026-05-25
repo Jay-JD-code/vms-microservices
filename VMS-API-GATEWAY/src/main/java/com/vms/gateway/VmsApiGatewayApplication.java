14ee8daab6d6da2d5c1103c2c545fbed54a128a3
Jay Deshmukh <jaydeshmukh0708@gmail.com>
2026-05-25 14:25:56 +0530

    Fix DataSourceAutoConfiguration import path in Gateway and Dashboard

--- VMS-API-GATEWAY/src/main/java/com/vms/gateway/VmsApiGatewayApplication.java
+++ VMS-API-GATEWAY/src/main/java/com/vms/gateway/VmsApiGatewayApplication.java
@@ -3,7 +3,7 @@ package com.vms.gateway;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class VmsApiGatewayApplication {
